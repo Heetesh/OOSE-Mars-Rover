bd7720e4672b47e33a89b0ef4196ae8657db75b8
Heetesh Doorbiz <Heetesh@users.noreply.github.com>
2021-05-28 17:51:13 +0800

    Mars Rover API implementation

---
+++ src/main/java/edu/curtin/comp2003/roverapi/Sensors.java
@@ -0,0 +1,28 @@
+package edu.curtin.comp2003.roverapi;
+
+public class Sensors
+{
+    /** Performs a temperature reading and returns the result in °C. */
+    public double readTemperature() {
+        // TODO: STUB
+        return 0.0;
+    }
+
+    /** Performs a visibility reading and returns the result in km. */
+    public double readVisibility() {
+        // TODO: STUB
+        return 0.0;
+    }
+
+    /** Performs a light-level reading, and returns the result in lux (units). */
+    public double readLightLevel() {
+        // TODO: STUB
+        return 0.0;
+    }
+
+    /** Takes a photo and returns the binary data making up the image. */
+    public byte[] takePhoto() {
+        // TODO: STUB
+        return null;
+    }
+}
---
+++ src/main/java/edu/curtin/comp2003/roverapi/SoilAnalyser.java
@@ -0,0 +1,23 @@
+package edu.curtin.comp2003.roverapi;
+
+public class SoilAnalyser {
+    /**
+     * Begins a soil analysis. The soil analysis will complete some time later,
+     * and its result can be retrieved by calling pollAnalysis().
+     *
+     * If startAnalysis() is called while analysis is already underway, it will
+     * throw an exception.
+     */
+    public void startAnalysis() {
+        // TODO: STUB
+    }
+
+    /**
+     * Retrieves the results of a soil analysis, if they're ready yet. If no new
+     * results have been produced, this method returns null.
+     */
+    public byte[] pollAnalysis() {
+        // TODO: STUB
+        return null;
+    }
+}
