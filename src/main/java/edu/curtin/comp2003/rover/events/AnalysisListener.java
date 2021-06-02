2c9b55a9bc4e4b79beba1d7012ff42e0947ac3fd
Heetesh Doorbiz <Heetesh@users.noreply.github.com>
2021-06-02 17:23:41 +0800

    Updated commenting

--- src/main/java/edu/curtin/comp2003/rover/events/AnalysisListener.java
+++ src/main/java/edu/curtin/comp2003/rover/events/AnalysisListener.java
@@ -1,4 +1,9 @@
 package edu.curtin.comp2003.rover.events;
 
+/**
+ * Interface extending an {@link EventListener EventListener} representing an analysis listener for the mars rover
+ * @see edu.curtin.comp2003.rover.core.MarsRover
+ * @author Heetesh Doorbiz
+ */
 public interface AnalysisListener extends EventListener {
 }
